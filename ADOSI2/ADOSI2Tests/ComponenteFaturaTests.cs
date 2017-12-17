17356a98ee6d98ceade041e72fd4b7dae8cb8530
André Gaudêncio <andregaudencio5@gmail.com>
2017-12-17 16:22:04 +0000

    added updatetest for ComponenteFaturaTests

--- ADOSI2/ADOSI2Tests/ComponenteFaturaTests.cs
+++ ADOSI2/ADOSI2Tests/ComponenteFaturaTests.cs
@@ -85,5 +85,92 @@ namespace ADOSI2Tests
                 }
             }
         }
+
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            using (Context ctx = new Context(connectionString))
+            {
+                Hóspede hóspede = new Hóspede();
+                hóspede.Bi = 1234567890;
+                hóspede.Nif = 0987654321;
+                hóspede.Nome = "Jaquim";
+                hóspede.Email = "jaquim@gmail.com";
+                hóspede.Morada = "Rua da Calçada";
+
+                HóspedeMapper hóspedeMapper = new HóspedeMapper(ctx);
+                hóspede = hóspedeMapper.Create(hóspede);
+
+                Estada estada = new Estada();
+                estada.DataInicio = new DateTime(2007, 3, 1);
+                estada.DataFim = new DateTime(2017, 3, 1);
+                estada.Id = 25;
+                //TODO
+                estada.NifHospede = 0;
+
+                EstadaMapper estadaMapper = new EstadaMapper(ctx);
+                estada = estadaMapper.Create(estada);
+
+
+                Fatura fatura = new Fatura();
+                fatura.Hóspede = hóspede;
+                fatura.Estada = estada;
+                fatura.Id = 1;
+                fatura.ValorFinal = 0;
+
+
+                var faturaMapper = new FaturaMapper(ctx);
+                fatura = faturaMapper.Create(fatura);
+
+
+                ComponenteFatura componente = new ComponenteFatura();
+                componente.Preço = 25;
+                componente.Fatura = fatura;
+                componente.Tipo = "Alojamento";
+                componente.Descrição = "muito bom";
+                var componenteMapper = new ComponenteFaturaMapper(ctx);
+                componente = componenteMapper.Create(componente);
+
+                ComponenteFatura componente2 = componenteMapper.Read(componente.Id);
+                Assert.IsNotNull(componente2);
+
+
+                var componenteDescrição = "nada";
+                var extraAlojamento = "Extra Alojamento";
+                var componentePreço = 52;
+
+                componente.Descrição = componenteDescrição;
+                componente.Tipo = extraAlojamento;
+                componente.Preço = componentePreço;
+
+                componente=componenteMapper.Update(componente);
+
+                Assert.AreEqual(componente.Tipo,extraAlojamento);
+                Assert.AreEqual(componente.Descrição,componenteDescrição);
+                Assert.AreEqual(componente.Preço,componentePreço);
+
+
+                foreach (var e in componenteMapper.ReadAll())
+                {
+                    componenteMapper.Delete(e);
+                }
+
+                foreach (var e in faturaMapper.ReadAll())
+                {
+                    faturaMapper.Delete(e);
+                }
+
+                foreach (var e in hóspedeMapper.ReadAll())
+                {
+                    hóspedeMapper.Delete(e);
+                }
+
+                foreach (var e in estadaMapper.ReadAll())
+                {
+                    estadaMapper.Delete(e);
+                }
+            }
+        }
     }
 }
