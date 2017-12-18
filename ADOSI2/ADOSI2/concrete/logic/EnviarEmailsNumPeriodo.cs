03dffd05b11f0277454b557065a54626ab4b991d
André Gaudêncio <andregaudencio5@gmail.com>
2017-12-18 21:26:46 +0000

    Added Enviar Emails num Periodo and ListarAtividadesComLugares

---
+++ ADOSI2/ADOSI2/concrete/logic/EnviarEmailsNumPeriodo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace ADOSI2.concrete.logic
+{
+    class EnviarEmailsNumPeriodo
+    {
+        private readonly Context _context;
+
+        #region Helper
+        protected void EnsureContext()
+        {
+            if (_context == null)
+                throw new InvalidOperationException("Data Context not set.");
+        }
+
+
+        #endregion
+
+        public EnviarEmailsNumPeriodo(Context ctx)
+        {
+            _context = ctx;
+        }
+
+        /*
+         * return success
+         */
+
+        public bool Execute(int dias)
+        {
+            using (TransactionScope ts = new TransactionScope(TransactionScopeOption.Required))
+            {
+                EnsureContext();
+                _context.EnlistTransaction();
+                using (IDbCommand cmd = _context.CreateCommand())
+                {
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "enviarEmailsNumIntervaloTemporal";
+
+
+                    var diasSql = new SqlParameter("@dias", dias);
+                   
+                    cmd.Parameters.Add(diasSql);
+                    
+
+                    var affected = cmd.ExecuteNonQuery().ToString();
+                    cmd.Parameters.Clear();
+
+
+                    Console.WriteLine("{0} rows affected", affected);
+                }
+
+
+                ts.Complete();
+
+            }
+            return true;
+        }
+    }
+}
---
+++ ADOSI2/ADOSI2/concrete/logic/ListarAtividadeComLugares.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace ADOSI2.concrete.logic
+{
+    class ListarAtividadeComLugares
+    {
+        private readonly Context _context;
+
+        #region Helper
+        protected void EnsureContext()
+        {
+            if (_context == null)
+                throw new InvalidOperationException("Data Context not set.");
+        }
+
+
+        #endregion
+
+        public ListarAtividadeComLugares(Context ctx)
+        {
+            _context = ctx;
+        }
+
+        /*
+         * return success
+         */
+
+        public bool Execute(DateTime dataInicio,DateTime dataFim)
+        {
+            using (TransactionScope ts = new TransactionScope(TransactionScopeOption.Required))
+            {
+                EnsureContext();
+                _context.EnlistTransaction();
+                using (IDbCommand cmd = _context.CreateCommand())
+                {
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "listarAtividadesComlugares";
+
+
+                    var dataInitSql = new SqlParameter("@dataInit", dataInicio);
+                    var dataFimSql = new SqlParameter("@dataFim", dataFim);
+
+                    cmd.Parameters.Add(dataInitSql);
+                    cmd.Parameters.Add(dataFimSql);
+
+
+                    var affected = cmd.ExecuteNonQuery().ToString();
+                    cmd.Parameters.Clear();
+
+
+                    Console.WriteLine("{0} rows affected", affected);
+                }
+
+
+                ts.Complete();
+
+            }
+            return true;
+        }
+    }
+}
