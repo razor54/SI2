e1993e37a1e5ba266fe3d988af4a2b4ca1bfc92c
ANDRE GAUDENCIO <andregaudencio5@gmail.com>
2017-12-20 22:49:15 +0000

    Added ApagarParqueEAssociaçoes and Tests

---
+++ ADOSI2/ADOSI2/concrete/logic/ApagarParqueEAssociaçoes.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using ADOSI2.mapper;
+using ADOSI2.model;
+
+namespace ADOSI2.concrete.logic
+{
+    public class ApagarParqueEAssociaçoes
+    {
+        private readonly Context _context;
+
+        #region Helper
+        protected void EnsureContext()
+        {
+            if (_context == null)
+                throw new InvalidOperationException("Data Context not set.");
+        }
+
+
+        #endregion
+
+        public ApagarParqueEAssociaçoes(Context ctx)
+        {
+            _context = ctx;
+        }
+
+        /*
+         * return success
+         */
+
+        public void Execute(Parque p)
+        {
+            using (TransactionScope ts = new TransactionScope(TransactionScopeOption.Required))
+            {
+                EnsureContext();
+                _context.EnlistTransaction();
+                using (IDbCommand cmd = _context.CreateCommand())
+                {
+
+                    var parqueMapper = new ParqueMapper(_context);
+                    var alojamentoMapper = new AlojamentoMapper(_context);
+                    var estadaMapper = new EstadaMapper(_context);
+                    var estadaAlojamentoMapper = new EstadaAlojamentoMapper(_context);
+                    var estadaHóspedeMapper = new EstadaHóspedeMapper(_context);
+                    var hospedeMapper = new HóspedeMapper(_context);
+
+                    var alojamentos = alojamentoMapper.ReadAll().Where(a => a.Parque.Nome.Equals(p.Nome));
+
+                    //find relations of alojamento-estada
+                    var estadaAlojamentos = estadaAlojamentoMapper.ReadAll()
+                        .Where(ea => alojamentos.Any(a=>a.Nome.Equals(ea.Alojamento.Nome)));
+
+                    var estadaHóspedes = estadaHóspedeMapper.ReadAll()
+                        .Where(eh => estadaAlojamentos.Any(al => al.Estada.Id == eh.Estada.Id));
+
+                    var hospedes = estadaHóspedes.Select(eh => eh.Hóspede);
+
+                    
+                    foreach (var estadaHóspede in estadaHóspedes)
+                    {
+                        estadaHóspedeMapper.Delete(estadaHóspede);
+                    }
+                   
+
+                   foreach (var ea in estadaAlojamentos)
+                   {
+                       estadaAlojamentoMapper.Delete(ea);
+                       estadaMapper.Delete(ea.Estada);
+                   }
+                    foreach (var alojamento in alojamentos)
+                    {
+                        alojamentoMapper.Delete(alojamento);
+                    }
+                    //alojamentos deleted
+
+                    var hospedesSemEstada =
+                        hospedes.Where(h => estadaHóspedeMapper.ReadAll().All(eh => eh.Hóspede.Nif != h.Nif));
+
+                    foreach (var e in hospedesSemEstada)
+                    {
+                        hospedeMapper.Delete(e);
+                    }
+
+                    parqueMapper.Delete(p);
+
+
+                }
+
+
+                ts.Complete();
+            }
+           
+        }
+    }
+}
---
+++ ADOSI2/ADOSI2Tests/ApagarParqueEAssociaçoesTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADOSI2.concrete;
+using ADOSI2.concrete.logic;
+using ADOSI2.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ADOSI2Tests
+{
+    [TestClass]
+    public class ApagarParqueEAssociaçoesTests
+    {
+        private readonly string _connectionString = ConfigurationManager.ConnectionStrings["si2cs"].ConnectionString;
+
+        [TestMethod]
+        public void ApagarParqueEAssociaçoesTest()
+        {
+            using (var ctx = new Context(_connectionString))
+            {
+                EstadaMapper estadaMapper = new EstadaMapper(ctx);
+                ParqueMapper parqueMap = new ParqueMapper(ctx);
+                AlojamentoMapper alojamentoMapper = new AlojamentoMapper(ctx);
+                var estadaAlojamentoMapper = new EstadaAlojamentoMapper(ctx);
+                try
+                {
+                    var estada = new Estada
+                    {
+                        DataInicio = new DateTime(2007, 3, 1),
+                        DataFim = new DateTime(2017, 3, 1),
+                        Id = 25,
+                        NifHospede = 0
+                    };
+
+                    
+                    estada = estadaMapper.Create(estada);
+
+
+                    /*
+                    * CREATE PARQUE
+                    */
+                    Parque p = new Parque();
+                    p.Nome = "brasil";
+                    p.Email = "brasil@brasil.com";
+                    p.Morada = "Rio de Janeiro, Rua Junqueiro 367";
+                    p.Estrelas = 5;
+
+                    
+                    p = parqueMap.Create(p);
+
+                    /*
+                     * Alojamento
+                     */
+
+
+                    Alojamento c = new Alojamento
+                    {
+                        PreçoBase = 50,
+                        Nome = "OI",
+                        Descrição = "sem descricao",
+                        Localizaçao = "Brasil",
+                        MaxPessoas = 20,
+                        Parque = p
+                    };
+
+                    
+                    c = alojamentoMapper.Create(c);
+
+
+                    EstadaAlojamento estadaAlojamento = new EstadaAlojamento()
+                    {
+                        Estada = estada,
+                        Alojamento = c,
+                        Descrição = "OLA",
+                        PreçoBase = 1234
+                    };
+
+                   
+
+                    estadaAlojamento = estadaAlojamentoMapper.Create(estadaAlojamento);
+
+
+                    var apagrParque = new ApagarParqueEAssociaçoes(ctx);
+                    apagrParque.Execute(p);
+
+                    var parqueExists = parqueMap.ReadAll().Any(parque => parque.Nome.Equals(p.Nome));
+                    Assert.AreEqual(false, parqueExists);
+
+                    var alojamentoExists = alojamentoMapper.ReadAll().Any(a => a.Nome.Equals(c.Nome));
+                    Assert.AreEqual(false, alojamentoExists);
+
+                    var estadaExists = estadaMapper.ReadAll().Any(e => e.Id == estada.Id);
+                    Assert.AreEqual(false, estadaExists);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+
+
+                    foreach (var e in estadaAlojamentoMapper.ReadAll())
+                    {
+                        estadaAlojamentoMapper.Delete(e);
+                    }
+
+                    foreach (var e in estadaMapper.ReadAll())
+                    {
+                        estadaMapper.Delete(e);
+                    }
+
+
+                    foreach (var alojamento in alojamentoMapper.ReadAll())
+                    {
+                        alojamentoMapper.Delete(alojamento);
+                    }
+
+
+                    foreach (var parque in parqueMap.ReadAll())
+                    {
+                        parqueMap.Delete(parque);
+                    }
+
+                    throw;
+                }
+            }
+        }
+    }
+}
